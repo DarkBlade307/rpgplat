cccced4c84523b3d7ce58715a0b5a7086c422bc6
DarkBlade307 <63086311+DarkBlade307@users.noreply.github.com>
2022-11-19 19:10:19 +1000

    added animations to the player
    he can slide, he can jump! He's supposed to rotate but for some reason... HE JUST WONT

--- Portals/Player.gd
+++ Portals/Player.gd
@@ -6,6 +6,8 @@ const MAXFALLSPEED = 100
 const MAXSPEED = 30
 const JUMPFORCE = 60
 
+var facing_right = true
+
 var motion = Vector2()
 
 func _ready():
@@ -18,13 +20,23 @@ func _physics_process(delta):
 		motion.y = MAXFALLSPEED
 	
 	if Input.is_action_pressed("right"):
+		facing_right = true
+		$AnimationPlayer.play("MoveRight")
 		motion.x = MAXSPEED
 	elif Input.is_action_pressed("left"):
+		facing_right = false
+		$AnimationPlayer.play("MoveLeft")
 		motion.x = -MAXSPEED
 	else:
 		motion.x = 0
+		$AnimationPlayer.play("Idle")
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -JUMPFORCE
+	else:
+		if facing_right == true:
+			$AnimationPlayer.play("AirRight")
+		elif facing_right == false:
+			$AnimationPlayer.play("AirLeft")
 			
 	motion = move_and_slide(motion, UP)
