2ca07dba6ab8f27dc415d7489249fff629e2defb
DarkBlade307 <63086311+DarkBlade307@users.noreply.github.com>
2022-11-19 21:05:24 +1000

    Add files via upload
    -added a new collision thingy just for hazards in the area
    -changed the collisions for the tiles and so now it looks 3d (WHOAH!! THE 3RD DIMENSION!!)
    -changed the players jump height by 0.5 (believe me it's a HUGE difference)
    -made some MAJOR edits to the level (still without a name)
    -made the player into its own scene so it can be updated across all levels without any hassle
    -added some new tiles to the tileset

---
+++ Portals/Portals/Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 2
+const MAXFALLSPEED = 100
+const MAXSPEED = 30
+const JUMPFORCE = 65
+
+var facing_right = true
+
+var motion = Vector2()
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	
+	if Input.is_action_pressed("right"):
+		facing_right = true
+		$AnimationPlayer.play("MoveRight")
+		motion.x = MAXSPEED
+	elif Input.is_action_pressed("left"):
+		facing_right = false
+		$AnimationPlayer.play("MoveLeft")
+		motion.x = -MAXSPEED
+	else:
+		motion.x = 0
+		$AnimationPlayer.play("Idle")
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -JUMPFORCE
+	else:
+		if facing_right == true:
+			$AnimationPlayer.play("AirRight")
+		elif facing_right == false:
+			$AnimationPlayer.play("AirLeft")
+			
+	motion = move_and_slide(motion, UP)
+
+
+func _on_DeathArea_body_entered(body):
+	if str(body) == "Player:[KinematicBody2D:1301]":
+		pass # Kill player code goes here
+		
+
