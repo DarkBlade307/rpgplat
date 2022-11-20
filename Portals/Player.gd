ea9835194b51d5cebd1904f324b2c58039fe6284
DarkBlade307 <63086311+DarkBlade307@users.noreply.github.com>
2022-11-20 10:01:08 +1000

    added some cool stuff
    -a skeleton head enemy (doesn't do anything yet)
    -hp system (you can't see it, but it's there)
    -an actual background (and the crowd goes wild)

--- Portals/Player.gd
+++ Portals/Player.gd
@@ -6,7 +6,16 @@ const MAXFALLSPEED = 100
 const MAXSPEED = 30
 const JUMPFORCE = 65
 
+signal health_updated(health)
+signal killed()
+
 var facing_right = true
+var prev_air = false
+
+export (float) var max_hp = 100
+
+onready var health = max_hp
+onready var iFrame = $Timer
 
 var motion = Vector2()
 
@@ -31,13 +40,17 @@ func _physics_process(delta):
 		motion.x = 0
 		$AnimationPlayer.play("Idle")
 	if is_on_floor():
+		prev_air = false
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -JUMPFORCE
 	else:
-		if facing_right == true:
-			$AnimationPlayer.play("AirRight")
-		elif facing_right == false:
-			$AnimationPlayer.play("AirLeft")
+		if prev_air == false:
+			if facing_right == true:
+				$AnimationPlayer.play("AirRight")
+			elif facing_right == false:
+				$AnimationPlayer.play_backwards("AirLeft")
+			prev_air = false
+		prev_air = false
 			
 	motion = move_and_slide(motion, UP)
 
@@ -46,4 +59,19 @@ func _on_DeathArea_body_entered(body):
 	if str(body) == "Player:[KinematicBody2D:1301]":
 		pass # Kill player code goes here
 		
-
+func kill():
+	pass
+	
+func damage(amount):
+	if iFrame.is_stopped():
+		iFrame.start()
+		set_health(health - amount)
+		
+func set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_hp)
+	if health != prev_health:
+		emit_signal("health_updated", health)
+		if health == 0:
+			kill()
+			emit_signal('killed')
